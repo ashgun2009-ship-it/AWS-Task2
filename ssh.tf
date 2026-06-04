e7d010f65fb2dcd89e0a355031d95c919d57b92c
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-04 09:38:54 +0300

    Add files via upload

--- ssh.tf
+++ ssh.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_version = ">= 1.5.7"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
 
 resource "aws_key_pair" "cmtr-o3e0v1ec-keypair" {
   key_name   = "cmtr-o3e0v1ec-keypair"
