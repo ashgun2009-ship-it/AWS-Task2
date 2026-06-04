ac5736f7752fb7b7ea0a3a768e40a85191085ee9
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-04 09:26:21 +0300

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_version = "1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
   required_version = ">= 1.5.7"
   required_providers {
     aws = {
