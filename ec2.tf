77b90be8b750045c87a1daa49d0884f0e083c281
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-04 12:45:37 +0300

    Add files via upload

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.5.7"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
 
 # Динамічно шукаємо VPC за тегом
 data "aws_vpc" "selected" {
--- variables.tf
+++ variables.tf
@@ -1,25 +1,5 @@
-terraform {
-  required_version = "1.5.7"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-terraform {
-  required_version = ">= 1.5.7"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 variable "ssh_key" {
   type        = string
   description = "Provides custom public SSH key."
+  default     = ""
 }
