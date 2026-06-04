9a87376561459c1ed8649440b8f8992ff549eb26
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-04 09:06:58 +0300

    Add files via upload

--- ec2.tf
+++ ec2.tf
@@ -1,36 +1,15 @@
-# 1. Шукаємо існуючу VPC за тегом
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = ["cmtr-o3e0v1ec-vpc"]
-  }
-}
-
-# 2. Шукаємо публічну підмережу всередині цієї VPC
-data "aws_subnet" "selected" {
-  vpc_id = data.aws_vpc.selected.id
-  filter {
-    name   = "tag:Name"
-    values = ["cmtr-o3e0v1ec-subnet-public-a"] # стандартне ім'я публічної підмережі на платформі
-  }
-}
-
-# 3. Шукаємо готову групу безпеки за її точним іменем з ТЗ
-data "aws_security_group" "selected" {
-  vpc_id = data.aws_vpc.selected.id
-  filter {
-    name   = "group-name"
-    values = ["cmtr-o3e0v1ec-sg"]
-  }
-}
-
-# 4. Створюємо екземпляр EC2
 resource "aws_instance" "web" {
-  ami                         = "ami-0905a3c97561e0b69" # Ubuntu 22.04 LTS в eu-west-1
-  instance_type               = "t2.micro"
-  subnet_id                   = data.aws_subnet.selected.id
-  vpc_security_group_ids      = [data.aws_security_group.selected.id]
-  key_name                    = aws_key_pair.auth.key_name
+  ami           = "ami-0905a3c97561e0b69" # Чиста Ubuntu 22.04 LTS в eu-west-1
+  instance_type = "t2.micro"
+
+  # Публічна підмережа, яку ми знайшли на твоєму скриншоті
+  subnet_id = "subnet-07464cc4c732baa90"
+
+  # СЮДИ ВСТАВ РЕАЛЬНИЙ ID СВОЄЇ ГРУПИ БЕЗПЕКИ З КОНСОЛІ AWS:
+  vpc_security_group_ids = ["sg-0176a1ac997e1ee59"]
+
+  # Кажемо серверу використовувати ключ по його імені (Terraform не буде його створювати)
+  key_name                    = aws_key_pair.cmtr-o3e0v1ec-keypair.key_name
   associate_public_ip_address = true
 
   tags = {
--- ssh.tf
+++ ssh.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_key_pair" "auth" {
+resource "aws_key_pair" "cmtr-o3e0v1ec-keypair" {
   key_name   = "cmtr-o3e0v1ec-keypair"
   public_key = var.ssh_key
 
