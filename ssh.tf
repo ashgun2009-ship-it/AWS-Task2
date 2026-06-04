c29e775b96578acb164674f0b245a7bfa8ff5a7c
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-04 09:22:08 +0300

    Add files via upload

--- ssh.tf
+++ ssh.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.5.7"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-} # Тут обов'язково має бути перенос рядка (Enter)
-
-provider "aws" {
-  region = "eu-west-1"
-}
 
 resource "aws_key_pair" "cmtr-o3e0v1ec-keypair" {
   key_name   = "cmtr-o3e0v1ec-keypair"
