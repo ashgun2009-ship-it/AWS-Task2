47c424e95f7202ef84dc3b3ad4f597e231ac6845
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-04 09:16:51 +0300

    Add files via upload

--- ssh.tf
+++ ssh.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+} # Тут обов'язково має бути перенос рядка (Enter)
+
 provider "aws" {
   region = "eu-west-1"
 }
